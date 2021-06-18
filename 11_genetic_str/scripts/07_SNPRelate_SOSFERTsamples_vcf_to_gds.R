5047f5da5a9a034485a0ffe8b1a3ee446a493039
sergiopalmavera <palma-vera@linu6.fbn-dummerstorf.de>
2021-06-18 16:48:08 +0200

    07_SNPRelate_SOSFERTsamples_vcf_to_gds.R created

---
+++ 11_genetic_str/scripts/07_SNPRelate_SOSFERTsamples_vcf_to_gds.R
@@ -0,0 +1,16 @@
+# Checking example before running SNPRelate on my data
+## According to https://bioconductor.org/packages/release/bioc/vignettes/SNPRelate/inst/doc/SNPRelateTutorial.html#installation-of-the-package-snprelate
+
+# Load packages
+library(gdsfmt)
+library(SNPRelate)
+library(SeqArray)
+
+# convert VCF to GDS
+vcf <- "../../10_FinalVCF/output/cohort_biallelicSNPs_HardFiltered_WithMissingness_SOSFERTsamples_GTfiltered.vcf"
+seqVCF2GDS(vcf, "../output/cohort_biallelicSNPs_HardFiltered_WithMissingness_SOSFERTsamples_GTfiltered.gds")
+
+
+#seqVCF2GDS("tst.vcf", "tst.gds")
+
+
